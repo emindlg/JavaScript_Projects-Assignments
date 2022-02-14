434db312e5c8bc6c1db00834fbd9558609593dd9
emindlg <94062446+emindlg@users.noreply.github.com>
2022-02-14 22:13:21 +0300

    2. commit

--- Find the Number Game/app.js
+++ Find the Number Game/app.js
@@ -7,31 +7,72 @@ console.log(control);
 let sonuc = document.getElementById("sonucyazısı");
 console.log(sonuc);
 
-const rndmsayı = Math.floor((Math.random()*10) + 1);
+let kalanhak1 = document.getElementById("kalanhak");
 
-hak = 3;
+let kaldıhakk = document.getElementById("hakkımkaldı")
+
+
+const rndmsayı = Math.floor((Math.random()*30) + 1);
+
+
+
+let hak = 5;
 
 
 
 
 control.addEventListener("click", ()=>{
 
-   
+       
     
-    if (enterNumber.value == rndmsayı && hak) {
-
-        sonuc.innerText = ("Tebrikler");
+    
+    if (enterNumber.value == rndmsayı) {
         
-    } else if (enterNumber.value > rndmsayı) {
-        sonuc.innerText = ("Biraz aşağı");
-         
+        sonuc.innerText = ("Tebrikler");
+        //kalanhak1.innerText = (`deneme sayısı ${sayac}`);
+        kaldıhakk.innerText = (`${5 - hak}. hakkınızda bildiniz :) `)
+        kalanhak1.innerText = ""
+        
+        
+        
     } 
-      else if (enterNumber.value < rndmsayı )
-    {
-        sonuc.innerText = ("Biraz yukarı");
+    
+    else {
+
+        hak -=1;
+
+        if (hak == 0){
+            enterNumber.disabled = true;
+            kaldıhakk.innerText = ( "Üzgünüm..Hakkınız bitti :(" );
+            sonuc.innerText = "";
+            kalanhak1.innerText = ""
+            
+            
+
+            
+        }
+        
+        
+        else if (enterNumber.value > rndmsayı) {
+            sonuc.innerText = ("Biraz aşağı");
+            
+            kaldıhakk.innerText = (`${hak} deneme hakkınız kaldı`)
+            kalanhak1.innerText = (`Lütfen 0 ile ${enterNumber.value} arasında bir sayı girin.`);
+            
+             
+        } 
+          else if (enterNumber.value < rndmsayı )
+        {
+            sonuc.innerText = ("Biraz yukarı");
+            kalanhak1.innerText = (`Lütfen ${enterNumber.value} ile 30 arasında bir sayı girin.`);
+            kaldıhakk.innerText = (`${hak} deneme hakkınız kaldı`)
+        }
     }
+    
+    
        
 })
+
 
 
 
