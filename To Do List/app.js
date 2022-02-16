81ef47b329ebdbbc880ab04833dfa8a8092ab822
emindlg <94062446+emindlg@users.noreply.github.com>
2022-02-16 23:47:00 +0300

    completed

--- To Do List/app.js
+++ To Do List/app.js
@@ -1,52 +1,28 @@
-let icon = document.querySelector(".fas");
+let icon = document.querySelector("#plus");
 let inputform1 = document.querySelector("#inputform");
 let taskToDo = document.querySelector(".taskToDo");
 let container = document.querySelector(".container");
+let trash = document.querySelector("#trash");
 
 
 
-icon.addEventListener("click", ()=>{
+icon.addEventListener("click", ()=>{    
     let paragraph = document.createElement('p');
     taskToDo.appendChild(paragraph);    
     paragraph.innerHTML = inputform1.value;
     inputform1.value = "";
 
-});
-    
-    
-    
-//icon.addEventListener("click", ()=>{
-//    let paragraph = document.createElement  ('p');        
-//    taskToDo.remove('p'); 
-//    alert("please enter your task to the texarea...");
-//});
+        paragraph.addEventListener("click", ()=>{
+            paragraph.style.textDecoration = 'line-through'; 
+            
+                trash.addEventListener("click", ()=>{
+                    
+                    taskToDo.removeChild(paragraph);
+                });
 
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function empty(inputform1) {
-    
-}
-
-
-icon.addEventListener("click", ()=>{
-    taskToDo.remove('p'); 
-    alert("please enter your tesk to the texarea...");
- }) 
+        });
+            
+            
+});                 
+                
+  
