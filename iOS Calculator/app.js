8938cdd299202b02f82902b3c7aa686b2fa0dd5a
emindlg <94062446+emindlg@users.noreply.github.com>
2022-02-18 16:02:04 +0300

    second

---
+++ iOS Calculator/app.js
@@ -0,0 +1,36 @@
+let numberValue = document.querySelectorAll('numbers');
+console.log(numberValue) ;
+let allOpt = document.querySelectorAll('.opt');
+let numInput1 = document.getElementById('small_number_input');
+let numInput2 = document.getElementById('big_number_input');
+let span = document.getElementsByTagName('span');
+
+
+//numberValue.addEventListener("click", ()=>{
+     
+//    console.log(numberValue) ;
+    
+//})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
