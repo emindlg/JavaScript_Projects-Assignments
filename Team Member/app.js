367653f7bef152c3832df593fe76af1c8084426c
emindlg <94062446+emindlg@users.noreply.github.com>
2022-03-17 13:24:42 +0300

    done

---
+++ Team Member/app.js
@@ -0,0 +1,30 @@
+let bdy = document.querySelector("#bodySize");
+let swch = document.getElementById("flexSwitchCheckDefault");
+let card = document.getElementById("card");
+
+swch.addEventListener("click", () =>{
+    bdy.classList.toggle("darkmode");
+    
+
+    //card.forEach((element) => {
+    //    let yeni ="" ;
+    //    for (let i = 0; i < card.length; i++) {
+    //       let element = card[i];
+    //   }
+        
+    //});
+    //card.classList.toggle("darkDiv");
+    
+    
+    
+
+});
+
+
+console.log(card);
+
+
+//for (let index = 0; index < array.length; index++) {
+//    const element = array[index];
+    
+//}
